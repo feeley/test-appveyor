3fcbe80b72d91036eb2b20a8484ca6615b783dde
Marc Feeley <feeley@iro.umontreal.ca>
2016-05-06 08:33:21 -0400

    check fp precision

--- foo.c
+++ foo.c
@@ -7,9 +7,13 @@ double c = 1.734723475976807e-18;
 int main() {
   double x = a + b*c;
   printf("sizeof(void*)=%lu\n",sizeof(void*));
-  printf("%g\n",a);
-  printf("%g\n",b);
-  printf("%g\n",c);
-  printf("%g\n",x);
+  printf("%f\n",a);
+  printf("%f\n",b);
+  printf("%f\n",c);
+  printf("%f\n",x);
+  printf("0x%016llx\n",*(long long*)&a);
+  printf("0x%016llx\n",*(long long*)&b);
+  printf("0x%016llx\n",*(long long*)&c);
+  printf("0x%016llx\n",*(long long*)&x);
   return 0;
 }
