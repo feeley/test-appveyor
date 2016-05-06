54e01709a5a985fe175092639bbcb5cc6d528760
Marc Feeley <feeley@iro.umontreal.ca>
2016-05-06 07:12:23 -0400

    check fp precision

--- foo.c
+++ foo.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
+double a = 0.01;
+double b = 4.00;
+double c = 1.734723475976807e-18;
+
 int main() {
-  printf("sizeof(void*)=%d\n",sizeof(void*));
+  double x = a + b*c;
+  printf("sizeof(void*)=%lu\n",sizeof(void*));
+  printf("%g\n",a);
+  printf("%g\n",b);
+  printf("%g\n",c);
+  printf("%g\n",x);
   return 0;
 }
--- makefile
+++ makefile
@@ -1,5 +1,3 @@
 all:
-	echo "abc" | sed -e "s/b/XXX/g"
-	which $(CC)
 	$(CC) -v
 	$(CC) -o foo.exe foo.c
